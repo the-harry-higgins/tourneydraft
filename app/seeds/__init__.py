edbd52ac9722e195b62b1a2e3631225a86a12317
Harrison Higgins <harryrhiggins@gmail.com>
2021-03-17 01:49:18 -0600

    Added data for 2021 tournament

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,12 +5,16 @@ from .colleges import seed_colleges, undo_colleges
 from .tournament_2018 import seed_2018_tournament, undo_2018_tournament
 from .tournament_2019 import seed_2019_tournament, undo_2019_tournament
 from .draft_2019 import seed_draft_2019, undo_draft_2019
+from .colleges_2021 import seed_colleges_2021, undo_colleges_2021
+from .tournament_2021 import seed_2021_tournament, undo_2021_tournament
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 # Creates the `flask seed all` command
+
+
 @seed_commands.command('all')
 def seed():
     seed_dumb_friends_league()
@@ -21,6 +25,8 @@ def seed():
     seed_draft_2019()
 
 # Creates the `flask seed undo` command
+
+
 @seed_commands.command('undo')
 def undo():
     undo_dumb_friends_league()
@@ -29,3 +35,15 @@ def undo():
     undo_2018_tournament()
     undo_2019_tournament()
     undo_draft_2019()
+
+
+@seed_commands.command('2021')
+def seed_2021():
+    seed_colleges_2021()
+    seed_2021_tournament()
+
+
+@seed_commands.command('undo-2021')
+def undo_2021():
+    undo_colleges_2021()
+    undo_2021_tournament()
--- react-app/src/components/BracketRegion.js
+++ react-app/src/components/BracketRegion.js
@@ -1,13 +1,13 @@
-import { Typography } from '@material-ui/core';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Game from './Game';
+import { Typography } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import Game from "./Game";
 
 function getRoundGameNums(regionNum, roundNum) {
-  const games = [0, 8, 4, 2, 1]
-  const start = [0, 1, 33, 49, 57]
-  return [...Array(games[roundNum]).keys()].map(num => {
-    return num + start[roundNum] + (regionNum - 1) * games[roundNum]
+  const games = [0, 8, 4, 2, 1];
+  const start = [0, 1, 33, 49, 57];
+  return [...Array(games[roundNum]).keys()].map((num) => {
+    return num + start[roundNum] + (regionNum - 1) * games[roundNum];
   });
 }
 
@@ -17,16 +17,33 @@ function getGameByNum(games, num) {
   }
 }
 
-const links = ['link1to9', 'link2to9', 'link3to10', 'link4to10', 'link5to11', 'link6to11', 'link7to12', 'link8to12', 'link9to13', 'link10to13', 'link11to14', 'link12to14', 'link13to15', 'link14to15',]
+const links = [
+  "link1to9",
+  "link2to9",
+  "link3to10",
+  "link4to10",
+  "link5to11",
+  "link6to11",
+  "link7to12",
+  "link8to12",
+  "link9to13",
+  "link10to13",
+  "link11to14",
+  "link12to14",
+  "link13to15",
+  "link14to15",
+];
 
 export default function BracketRegion(props) {
-  const games = useSelector(state => state.entities.games);
-  const roundNum = useSelector(state => state.ui.roundNum);
-  const marchMadnessTeams = useSelector(state => state.entities.marchMadnessTeams);
+  const games = useSelector((state) => state.entities.games);
+  const roundNum = useSelector((state) => state.ui.roundNum);
+  const marchMadnessTeams = useSelector(
+    (state) => state.entities.marchMadnessTeams
+  );
 
   const createGames = (thisRound) => {
     const roundGameNums = getRoundGameNums(props.regionNum, thisRound);
-    const gameOffsets = [0, 1, 9, 13, 15]
+    const gameOffsets = [0, 1, 9, 13, 15];
     // Games without teams
     if (thisRound > roundNum) {
       return roundGameNums.map((num, index) => (
@@ -46,12 +63,10 @@ export default function BracketRegion(props) {
           bracketGameNum={index + gameOffsets[thisRound]}
           thisRound={thisRound}
           roundNum={roundNum}
-          teams={
-            [
-              marchMadnessTeams[getGameByNum(games, num).team_ids[0]],
-              marchMadnessTeams[getGameByNum(games, num).team_ids[1]]
-            ]
-          }
+          teams={[
+            marchMadnessTeams[getGameByNum(games, num).team_ids[0]],
+            marchMadnessTeams[getGameByNum(games, num).team_ids[1]],
+          ]}
           color={false}
         />
       ));
@@ -62,42 +77,44 @@ export default function BracketRegion(props) {
           bracketGameNum={index + gameOffsets[thisRound]}
           thisRound={thisRound}
           roundNum={roundNum}
-          teams={
-            [
-              marchMadnessTeams[getGameByNum(games, num).team_ids[0]],
-              marchMadnessTeams[getGameByNum(games, num).team_ids[1]]
-            ]
-          }
+          teams={[
+            marchMadnessTeams[getGameByNum(games, num).team_ids[0]],
+            marchMadnessTeams[getGameByNum(games, num).team_ids[1]],
+          ]}
           game={getGameByNum(games, num)}
           color={true}
         />
       ));
     }
-  }
+  };
 
   return (
-    <div className={props.regionNum < 3 ? 'bracket-region__left' : 'bracket-region__right'}>
-      <div className='region'>
-        <Typography variant='h3'>{props.region}</Typography>
+    <div
+      className={
+        props.regionNum < 3 ? "bracket-region__left" : "bracket-region__right"
+      }
+    >
+      <div className="region">
+        <Typography variant="h3">{props.region}</Typography>
       </div>
-      <div className='round1'>
-        <Typography variant='subtitle1'>Round 1</Typography>
+      <div className="round1">
+        <Typography variant="subtitle1">Round 1</Typography>
       </div>
-      <div className='round2'>
-        <Typography variant='subtitle1'>Round 2</Typography>
+      <div className="round2">
+        <Typography variant="subtitle1">Round 2</Typography>
       </div>
-      <div className='round3'>
-        <Typography variant='subtitle1'>Sweet Sixteen</Typography>
+      <div className="round3">
+        <Typography variant="subtitle1">Sweet Sixteen</Typography>
       </div>
-      <div className='round4'>
-        <Typography variant='subtitle1'>Elite Eight</Typography>
+      <div className="round4">
+        <Typography variant="subtitle1">Elite Eight</Typography>
       </div>
-      { createGames(1) }
-      { createGames(2) }
-      { createGames(3) }
-      { createGames(4) }
-      { links.map(link => (
-        <div key={link} className={link}/>
+      {createGames(1)}
+      {createGames(2)}
+      {createGames(3)}
+      {createGames(4)}
+      {links.map((link) => (
+        <div key={link} className={link} />
       ))}
     </div>
   );
